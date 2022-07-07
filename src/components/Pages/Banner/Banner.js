d19aca1bddcd00dc7362a61f61f2b1a58583ccf4
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-07 16:52:16 +0600

    home pages banner

--- src/components/Pages/Banner/Banner.js
+++ src/components/Pages/Banner/Banner.js
@@ -1,13 +1,32 @@
 import React from 'react';
-
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { BsArrowDown } from "react-icons/bs";
+import banner1 from '../../Images/Banner-1.png'
+import banner2 from '../../Images/Banner-2.png'
 const Banner = () => {
     return (
-        <div className='w-full h-[91vh]'>
-            <h1 className='text-9xl font-bold'>Experience + <span className='text-slate-400'>Creative</span></h1>
-            <div>
-                <p className='text-2xl text-slate-600'>We're an innovative design agency building high-end products <br />and experiences that grow your business exponentially</p>
-
+        <div className='w-full h-100 md:h-[91vh] relative'>
+            <h1 className='text-9xl font-bold'>Experience + <span className='text-slate-400'>Creativ</span></h1>
+            <p className='text-2xl text-slate-600'>We're an innovative design agency building high-end products <br />and experiences that grow your business exponentially</p>
+            <div className='mt-16 flex justify-around items-center'>
+                <div>
+                    <button className=' flex justify-center items-center gap-3 text-2xl font-medium px-8 py-3 bg-slate-700 uppercase rounded-sm'>view our works <span><AiOutlineArrowRight /></span> </button>
+                </div>
+                <button className='bg-green-500 text-black p-10 text-5xl rounded-full'><BsArrowDown /></button>
             </div>
+            <div className='hidden md:block'>
+                <div className='flex justify-between'>
+                    <div></div>
+                    <div className='absolute bottom-0 right-[-32px]'>
+                        <div className='flex items-center gap-4 '>
+                            <h1 className=' text-slate-600'>RECENT PROJECT</h1>
+                            <img className='w-80' src={banner1} alt="Banner Images" />
+                            <img className='w-80' src={banner2} alt="Banner Images " />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
