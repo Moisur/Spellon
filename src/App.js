dfc108579c73c4d5b5283755404d801b1f000da1
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-07 16:01:31 +0600

    react animation coursor pointer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import Home from "./components/Pages/Home/Home";
-
+import AnimatedCursor from "react-animated-cursor"
 function App() {
   return (
     <div>
       <Home></Home>
+      <AnimatedCursor
+        innerSize={15}
+        outerSize={8}
+        color='255, 255, 0'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
     </div>
   );
 }
